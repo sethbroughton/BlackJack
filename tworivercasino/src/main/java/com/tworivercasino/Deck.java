0f7159f3b3850a36f6bd693211c760e9c7c1f87f
Seth Broughton <broughton.24@gmail.com>
2020-02-08 12:30:22 -0500

    update

--- tworivercasino/src/main/java/com/tworivercasino/Deck.java
+++ tworivercasino/src/main/java/com/tworivercasino/Deck.java
@@ -1,5 +1,39 @@
 package com.tworivercasino;
 
-public class Deck {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Deck {
+	
+	public static final int CARDS_IN_DECK = 52;
+	private List<Card> listOfCards = new ArrayList<Card>();
+	
+	//constructor
+	public Deck(){
+	}
+	
+	//methods
+
+	public void buildDeck() {
+
+		String[] suits = {"H","D","S","C"};
+		String[] ranks = {"A","2","3","4","5","6","7","8","9","T","J","Q","K"};
+		for(String suit : suits) {
+			for(String rank : ranks) {
+				Card currentCard = new Card();
+				currentCard.setSuit(suit);
+				currentCard.setRank(rank);
+				listOfCards.add(currentCard);
+			}			
+		}
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(listOfCards);
+	}
+	
+	
+
+	
 }
