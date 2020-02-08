bc31381fe30a40d69401feb809595960b44885df
Seth Broughton <broughton.24@gmail.com>
2020-02-08 11:58:54 -0500

    Initial Commit

---
+++ CardGame/src/com/tworiverscasino/Card.java
@@ -0,0 +1,46 @@
+package com.tworiverscasino;
+
+public class Card {
+	private String rank;
+	private String suit;
+	private boolean faceUp;
+	
+	//getter
+	public String getRank() {
+		return rank;
+	}
+	public String getSuit() {
+		return suit;
+	}
+	public boolean isFaceUp() {
+		return faceUp;
+	}
+	
+	//setters
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+	
+	//constructor
+	public Card(){
+		
+	}
+	
+	//methods - flip card
+	
+	public void flipCard() {
+		if(faceUp) {
+			faceUp = false;
+		}
+		else {
+			faceUp = true;
+		}
+	}
+	
+	
+	
+	
+}
---
+++ CardGame/src/com/tworiverscasino/MainGame.java
@@ -0,0 +1,11 @@
+package com.tworiverscasino;
+
+public class MainGame {
+
+	public static void main(String[] args) {
+		
+		System.out.println(listOfCards);
+		
+	}
+
+}
